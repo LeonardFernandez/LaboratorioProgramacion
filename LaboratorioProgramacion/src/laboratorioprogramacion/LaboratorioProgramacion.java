92e30afebea6bfc79f6a3dd4f21d98c5c16129ce
LeonardFernandez <leonard.fernandez@est.fi.uncoma.edu.ar>
2024-08-15 18:51:46 -0300

    cambio en feature

--- LaboratorioProgramacion/src/laboratorioprogramacion/LaboratorioProgramacion.java
+++ LaboratorioProgramacion/src/laboratorioprogramacion/LaboratorioProgramacion.java
@@ -16,8 +16,8 @@ public class LaboratorioProgramacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Rama feature");
+        //Rama feature
     }
     
 }
